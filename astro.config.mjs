4db1a3e98aa59784217c1b817134db786ec5030d
xia <natsu_summer_x@outlook.com>
2026-03-17 17:12:08 +0800

    Build three-column blog layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 export default defineConfig({
   site: "https://ddy314.github.io/myblog/",
   base: "/myblog",
   output: "static",
+  image: {
+    service: passthroughImageService(),
+  },
 });
